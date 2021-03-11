b99a48aafc9ea390b4422f50b28bc16de343dd04
vutr <me@vutr.io>
2021-03-11 15:33:52 +0700

    update frontend

--- webapp/src/maths.ts
+++ webapp/src/maths.ts
@@ -1,6 +1,9 @@
 import { Point } from './types'
 
-
+/*
+  Convert an angle(Degrees) to Point A's coordinates(X, Y) in a layout,
+  with a given origin O and distance(radius) from the Point A to O
+ */
 export const angle_to_coord = (angle: number, radius: number, origin: Point): Point => {
   const pi_angle = angle * Math.PI / 180
   const x = radius * Math.sin(pi_angle) + origin.x
